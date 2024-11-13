1bcddb9e62b4185c71a210208f7e0ae21d2f1c4b
CrysDrag <tungphamj@gmail.com>
2024-11-13 08:49:27 +0700

    final

--- js/compareBox.js
+++ js/compareBox.js
@@ -1,6 +1,7 @@
 // const phoneData = {}; //biến lưu dữ liệu đọc từ file
 const listPhone = []; // mảng lưu những phone cần so sánh
 const max = 3; //giới hạn phone so sánh
+const bodyDir = document.getElementsByTagName('body')[0];
 
 //tạo nút hiển thị số điện thoại nằm trong list so sánh và làm hiện những điện thoại đang trong danh sách
 const isComparing = document.createElement('button');
@@ -68,7 +69,7 @@ compare.innerHTML = `
     </div>
 
     <div class="confirmBtnBox">
-        <button id="confirmBtn" >So sánh!</button>
+        <button id="confirmBtn" disabled>So sánh!</button>
         <button id="removeListBtn" >Xóa toàn bộ sanh sách</button>
     </div>
     <div class="collapseBtn">
@@ -79,8 +80,8 @@ compare.innerHTML = `
 compare.classList.add('compareBox');
 compare.id = 'compareBox';
 compare.style.display = 'none';
-document.getElementsByTagName('body')[0].appendChild(compare);
-document.getElementsByTagName('body')[0].appendChild(isComparing);
+bodyDir.appendChild(compare);
+bodyDir.appendChild(isComparing);
 
 //nút "xóa toàn bộ danh sách"
 const check = document.getElementById('removeListBtn');
@@ -124,8 +125,7 @@ function showToast(message) {
 
     toast.style.bottom = `165px`
 
-    const container = document.getElementsByTagName("body");
-    container[0].appendChild(toast);
+    bodyDir.appendChild(toast);
 
     setTimeout(() => toast.classList.add("show"), 100);
 
@@ -189,7 +189,7 @@ function renderChoosingPhone() {
         document.getElementById(`compareBtn${i + 1}`).style.display = 'flex';
         document.getElementById(`choosenBoxPhone${i + 1}`).style.display = 'none';
     }
-    
+
     listPhone.forEach((phone, index) => {
         const divToHide = document.getElementById(`compareBtn${index + 1}`);
         divToHide.style.display = 'none';
@@ -201,7 +201,7 @@ function renderChoosingPhone() {
             deleteProduct(phone);
 
             const oldCB = document.getElementById(`${phone.brand}_${phone.id}`);
-            if(oldCB) oldCB.checked = false;
+            if (oldCB) oldCB.checked = false;
         };
 
         divToShow.children[1].src = phone.image;
@@ -218,27 +218,110 @@ function renderChoosingPhone() {
 const compareBtn = document.getElementById('confirmBtn');
 compareBtn.onclick = () => {
     sessionStorage.setItem('listPhone', JSON.stringify(listPhone));
-    window.open('/comparePhone.html', '_blank');
+    const repoName = window.location.pathname.split('/')[1];
+    window.open(`/comparePhone.html`, '_blank');
     // renderCompareData();
 }
 ////////bên này là file comparePhone.html dùng để hiển thị những phone được confirm là so sánh
+
+//function xử lý responsive
+function reSize(listPhone) {
+    if (window.innerWidth < 680) {
+        if (listPhone.length < 3) {
+            for (let j = listPhone.length + 1; j < 4; j++) {
+                for (let i = 0; i < table.rows.length; i++) {
+                    const cellDisplay = table.rows[i].cells[j];
+                    if (cellDisplay) cellDisplay.style.display = 'none';
+                }
+                const pHeader = document.getElementById(`phone${j}2`);
+                if (pHeader) pHeader.style.display = 'none';
+            }
+        }
+        toObverse(0.5);
+    } else {
+        for (let j = listPhone.length + 1; j < 4; j++) {
+            for (let i = 0; i < table.rows.length; i++) {
+                const cellDisplay = table.rows[i].cells[j];
+                if (cellDisplay) cellDisplay.style.display = 'table-cell';
+            }
+            const pHeader = document.getElementById(`phone${j}2`);
+            if (pHeader) pHeader.style.display = 'block';
+        }
+    }
+}
+
+let observer;
+function toObverse(amount) {
+    if (observer) observer.disconnect();
+
+    observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            const phoneH = document.getElementById('phoneHeader');  // ID của div2
+            if (entry.isIntersecting) {
+                // Nếu div1 nằm trong viewport
+                phoneH.style.display = 'none';  // Ẩn div2
+            } else {
+                // Nếu div1 không nằm trong viewport
+                phoneH.style.display = 'flex';  // Hiển thị div2
+            }
+        });
+    }, {
+        threshold: amount  // Phần tử phải chiếm ít nhất 10% diện tích viewport để được coi là "đã xuất hiện"
+    });
+
+    const tHead = document.getElementsByTagName('thead')[0];
+    observer.observe(tHead);
+}
+
+function phoneHeader() {
+    const phoneHeader = document.createElement('div');
+    phoneHeader.classList.add('phoneHeader');
+    phoneHeader.id = 'phoneHeader';
+    phoneHeader.style.width = `${table.clientWidth}px`;
+    phoneHeader.innerHTML = `
+        <div id="notShow"></div>
+        <div id="phone12"></div>
+        <div id="phone22"></div>
+        <div id="phone32"></div>
+    `
+    bodyDir.appendChild(phoneHeader);
+
+
+    toObverse(0.6);
+}
 
 //hiển thị list được confirm
 function renderCompareData(listPhone) {
     const table = document.getElementById('table');
     const appleIcon = '<i class="fa-brands fa-apple"></i>';
-    const androidIcon = '<i class="fa-brands fa-android"></i>'
+    const androidIcon = '<i class="fa-brands fa-android"></i>';
+    const isResize = true;
 
     //clear toàn bộ các cell trong table trước khi render dữ liệu
-    for (let j = 1; j < 4; j++) {
+    for (let j = 1; j < listPhone.length; j++) {
         const mainCell = table.rows[0].cells[j];
         const div = mainCell.children[0];
         div.innerHTML = "";
-        for (let i = 2; i < 20; i++) {
+        for (let i = 2; i < table.rows.length; i++) {
             table.rows[i].cells[j].textContent = "";
             table.rows[i].cells[j].id = "";
+            table.rows[i].cells[j].style.display = 'table-cell';
         }
     }
+    window.addEventListener('resize', () => {
+        document.getElementById('phoneHeader').style.width = `${table.clientWidth}px`;
+        document.getElementById('notShow').style.width = `${document.getElementById('nothing').clientWidth}px`;
+
+        for (let i = 0; i < listPhone.length; i++) {
+            document.getElementById(`phone${i + 1}2`).style.width = `${document.getElementById('phone1').clientWidth}px`;
+        }
+        reSize(listPhone);
+    })
+    window.addEventListener('load', () => {
+        document.getElementById('notShow').style.width = `${document.getElementById('nothing').clientWidth}px`;
+
+        reSize(listPhone);
+    })
 
     //đọc dữ liệu từng phone và render theo cột
     listPhone.forEach((phone, index) => {
@@ -256,24 +339,44 @@ function renderCompareData(listPhone) {
                 <i class="fa-regular fa-circle-xmark i_hover"></i>
             </button>
         `
+
+        const div2 = document.getElementById(`phone${index + 1}2`);
+        div2.innerHTML = `
+            <img src="${phone.image}" alt="${phone.model} ${phone.storage}" class="imgPhone">
+            <span class="phoneName">${phone.model} ${phone.storage}</span>
+            <span class="phonePrice">${priceNum} VNĐ</span>
+            <span class="phoneColor">Màu sắc: ${phone.color}</span>
+            <button class="removeBtn" id="removeBtn${index + 1}2">
+                <i class="fa-solid fa-circle-xmark i_nor"></i>
+                <i class="fa-regular fa-circle-xmark i_hover"></i>
+            </button>
+        `
+        div2.style.width = `${document.getElementById('phone1').clientWidth}px`;
+
         document.getElementById(`removeBtn${index + 1}`).onclick = () => {
             listPhone.splice(index, 1);
             renderCompareData(listPhone);
         }
-        for (let i = 2; i < 30; i++) {
+
+        document.getElementById(`removeBtn${index + 1}2`).onclick = () => {
+            listPhone.splice(index, 1);
+            renderCompareData(listPhone);
+        }
+
+        for (let i = 2; i < table.rows.length; i++) {
             if (table.rows[i]) {
-                let name = table.rows[i].getAttribute('name');
+                let name = table.rows[i].getAttribute('name'); //lấy tên thuộc tính từng hàng
 
                 const cell = table.rows[i].cells[index + 1];
                 if (phone[name] === true) {
-                    cell.textContent = 'Có';
+                    cell.innerHTML = `<p class="normal">Có</p>`
                     cell.style.color = '#485fc7';
                 }
                 else if (phone[name] === false) {
-                    cell.textContent = 'Không';
+                    cell.innerHTML = `<p class="normal">Không</p>`
                     cell.style.color = 'red';
                 }
-                else cell.textContent = phone[name];
+                else cell.innerHTML = `<p class="normal">${phone[name]}</p>`;
 
                 if (name === 'operatingSystem') {
                     if (phone.brand === 'apple') {
@@ -281,7 +384,21 @@ function renderCompareData(listPhone) {
                     }
                     else cell.innerHTML = androidIcon + ' ' + cell.innerHTML;
                 }
+
                 cell.id = name + "_" + phone.brand + "_" + phone.id;
+
+                const labelOnHide = document.createElement('p');
+                labelOnHide.textContent = 'qwerty';
+                labelOnHide.classList.add('pHide');
+                cell.insertBefore(labelOnHide, cell.firstChild);
+
+                if (index === 0) {
+                    const labelOnHide = document.createElement('p');
+                    labelOnHide.innerHTML = table.rows[i].cells[index].innerHTML;
+                    labelOnHide.classList.add('pHide', 'pBold');
+                    cell.insertBefore(labelOnHide, cell.firstChild);
+                }
+
             }
         }
     })
@@ -290,6 +407,7 @@ function renderCompareData(listPhone) {
 
 if (window.location.pathname.endsWith('comparePhone.html')) {
     const listPhone = JSON.parse(sessionStorage.getItem('listPhone'));
+    phoneHeader();
     renderCompareData(listPhone);
 
     // window.addEventListener('pageshow', () => {
